c001437dcc68b4c360c293f7a0eea0038a470884
Sergio Rosa <scarlosjazz@gmail.com>
2019-03-30 12:50:13 -0400

    Components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const getCurrentDate = () => {
-    const date = new Date();
-    return date.toDateString();
-}
-const greeting = <h1>Hello World! Current date: {getCurrentDate()}</h1>;
 
 
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
+
+
+
+
+
+
+
+
+
+
+
+
+// const getCurrentDate = () => {
+//     const date = new Date();
+//     return date.toDateString();
+// }
+// const greeting = <h1>Hello World! Current date: {getCurrentDate()}</h1>;
